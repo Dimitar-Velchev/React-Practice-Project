dab190a98c77d79d74b1259b2e5d7ae2280bf7b3
Mitko <dimitarvelchev59@gmail.com>
2022-02-08 15:54:23 +0200

    Add dummy data

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -1,6 +1,10 @@
 import Player from "./components/Player";
 import Song from "./components/Song";
-import './styles/app.scss';
+import "./styles/app.scss";
+import data from "./data";
+
+
+
 function App() {
   return (
     <div className="App">
