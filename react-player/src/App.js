4a1f4844fac15b63bf68d1a45a149c5629bffef7
Mitko <dimitarvelchev59@gmail.com>
2022-02-08 17:46:06 +0200

    add start/end song time

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
 import "./styles/app.scss";
 import data from "./data";
 
-
-
 function App() {
+  const [songs, setSongs] = useState(data());
+
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
