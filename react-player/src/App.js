ab73f0d093b4b34dc4591dac2d3e0c19f90410e5
Mitko <dimitarvelchev59@gmail.com>
2022-02-18 14:19:51 +0200

    styling the input

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -19,6 +19,7 @@ function App() {
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
     duration: 0,
+    animationPercentage: 0,
   });
 
   const [libraryStatus, setLibraryStatus] = useState(false);
@@ -27,12 +28,21 @@ function App() {
     const current = e.target.currentTime;
     const duration = e.target.duration;
 
-    setSongInfo({ ...songInfo, currentTime: current, duration });
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100);
+    
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration,
+      animationPercentage: animation,
+    });
   };
 
   return (
     <div className="App">
-      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus}/>
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
       <Song currentSong={currentSong} />
       <Player
         audioRef={audioRef}
