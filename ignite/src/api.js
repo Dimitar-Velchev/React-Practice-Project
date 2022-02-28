87bf9df9f63b0a5d15599b99f571f1fcf2a68f83
Mitko <dimitarvelchev59@gmail.com>
2022-02-28 13:52:58 +0200

    Setup API

---
+++ ignite/src/api.js
@@ -0,0 +1,39 @@
+//Base URL
+const base_url = "https://api.rawg.io/api";
+const API_KEY = "82f28c739a4c4eee9af14a8d8670f3f9"
+
+//Getting date
+
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+//Popular Games
+
+const popular_games = `${base_url}/games?key=${API_KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+export const popularGamesURL = () => `${popular_games}`;
+
+
