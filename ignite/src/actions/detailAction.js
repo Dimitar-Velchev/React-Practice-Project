3efdeae0b1dfa644375fdcd2a5777ef774abc2fe
Mitko <dimitarvelchev59@gmail.com>
2022-03-13 18:01:22 +0200

    isLoading

--- ignite/src/actions/detailAction.js
+++ ignite/src/actions/detailAction.js
@@ -1,16 +1,19 @@
 import axios from "axios";
-import { gameDetailsURL,gameScreenShootURL } from "../api";
+import { gameDetailsURL, gameScreenShootURL } from "../api";
 
 export const loadDetail = (id) => async (dispatch) => {
+  dispatch({
+    type: "LOADING_DETAIL",
+  });
+
   const detailData = await axios.get(gameDetailsURL(id));
   const screenShotData = await axios.get(gameScreenShootURL(id));
-
 
   dispatch({
     type: "GET_DETAIL",
     payload: {
       game: detailData.data,
-      screen:screenShotData.data,
+      screen: screenShotData.data,
     },
   });
 };
